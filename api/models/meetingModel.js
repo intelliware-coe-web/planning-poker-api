70780abb86759af4c142fbd7dd634695f119aa1d
Linna Meng <mengl@mac-2387.local>
2019-05-24 11:47:00 -0400

    LM Added a test for meetings. Run npm test to run test

--- api/models/meetingModel.js
+++ api/models/meetingModel.js
@@ -3,15 +3,19 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var MeetingSchema = new Schema({
-  host: String,
-  attendees: [{
-    type: Schema.Types.ObjectId,
-    ref: 'User'
-  }],
-  created_date: {
-    type: Date,
-    default: Date.now
-  } 
+    name: {
+        type: String,
+        required: 'Name is required'
+    },
+    host: String,
+    attendees: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    created_date: {
+        type: Date,
+        default: Date.now
+    } 
 });
 
 module.exports = mongoose.model('Meeting', MeetingSchema);
