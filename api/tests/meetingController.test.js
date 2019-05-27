0e3fdd4ea645350e1b6968eea9044d0695d13612
Linna Meng <mengl@mac-2387.local>
2019-05-27 14:18:47 -0400

    LM Added controller test

---
+++ api/tests/meetingController.test.js
@@ -0,0 +1,20 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var app = require('../../index.js');
+var Meeting = require('../models/meetingModel');
+
+// Configure chai
+chai.use(chaiHttp);
+chai.should(); 
+
+describe('GET /meetings', function() {
+    it('should return all meetings', function(done) {
+        chai.request(app)
+            .get('/meetings')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                done();
+            });
+    });
+});
